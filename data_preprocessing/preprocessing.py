4390444a4c3fa0146150713fe66448ca089b56f4
rishavskz <rgupta@skoruz.com>
2020-07-05 18:28:29 +0530

    preprocessing file

---
+++ data_preprocessing/preprocessing.py
@@ -0,0 +1,89 @@
+import numpy as np
+import pandas as pd
+
+
+def prepare_data(drop_cols=True):
+    dependent_var = ['RUL']
+    index_columns_names = ["UnitNumber", "Cycle"]
+    operational_settings_columns_names = ["OpSet" + str(i) for i in range(1, 4)]
+    sensor_measure_columns_names = ["SensorMeasure" + str(i) for i in range(1, 22)]
+    input_file_column_names = index_columns_names + operational_settings_columns_names + sensor_measure_columns_names
+
+    cols_to_drop = ['OpSet3', 'SensorMeasure1', 'SensorMeasure5', 'SensorMeasure6', 'SensorMeasure10',
+                    'SensorMeasure14',
+                    'SensorMeasure16', 'SensorMeasure18', 'SensorMeasure19']
+
+    df_train = pd.read_csv('train_FD004.txt', delim_whitespace=True, names=input_file_column_names)
+
+    rul = pd.DataFrame(df_train.groupby('UnitNumber')['Cycle'].max()).reset_index()
+    rul.columns = ['UnitNumber', 'max']
+    df_train = df_train.merge(rul, on=['UnitNumber'], how='left')
+    df_train['RUL'] = df_train['max'] - df_train['Cycle']
+    df_train.drop('max', axis=1, inplace=True)
+
+    df_test = pd.read_csv('test_FD004.txt', delim_whitespace=True, names=input_file_column_names)
+
+    if drop_cols:
+        df_train = df_train.drop(cols_to_drop, axis=1)
+        df_test = df_test.drop(cols_to_drop, axis=1)
+
+    y_true = pd.read_csv('RUL_FD004.txt', delim_whitespace=True, names=["RUL"])
+    y_true["UnitNumber"] = y_true.index
+
+    return df_train, df_test, y_true
+
+
+'''
+LSTM expects an input in the shape of a numpy array of 3 dimensions that's why converting train and test data 
+accordingly.
+'''
+
+
+def gen_train(id_df, seq_length, seq_cols):
+    """
+        function to prepare train data into (samples, time steps, features)
+        id_df = train dataframe
+        seq_length = look back period
+        seq_cols = feature columns
+    """
+
+    data_array = id_df[seq_cols].values
+    num_elements = data_array.shape[0]
+    lstm_array = []
+
+    for start, stop in zip(range(0, num_elements - seq_length + 1), range(seq_length, num_elements + 1)):
+        lstm_array.append(data_array[start:stop, :])
+
+    return np.array(lstm_array)
+
+
+def gen_target(id_df, seq_length, label):
+    data_array = id_df[label].values
+    num_elements = data_array.shape[0]
+    return data_array[seq_length-1:num_elements+1]
+
+
+def gen_test(id_df, seq_length, seq_cols, mask_value):
+    """
+        function to prepare test data into (samples, time steps, features)
+        function only returns last sequence of data for every unit
+        id_df = test dataframe
+        seq_length = look back period
+        seq_cols = feature columns
+    """
+    df_mask = pd.DataFrame(np.zeros((seq_length - 1, id_df.shape[1])), columns=id_df.columns)
+    df_mask[:] = mask_value
+
+    id_df = df_mask.append(id_df, ignore_index=True)
+
+    data_array = id_df[seq_cols].values
+    num_elements = data_array.shape[0]
+    lstm_array = []
+
+    start = num_elements - seq_length
+    stop = num_elements
+
+    lstm_array.append(data_array[start:stop, :])
+
+    return np.array(lstm_array)
+
--- training/train.py
+++ training/train.py
@@ -11,32 +11,3 @@ from tensorflow.keras import callbacks
 
 
 # based on data wrangling python notebook
-def prepare_data(drop_cols=True):
-    dependent_var = ['RUL']
-    index_columns_names = ["UnitNumber", "Cycle"]
-    operational_settings_columns_names = ["OpSet" + str(i) for i in range(1, 4)]
-    sensor_measure_columns_names = ["SensorMeasure" + str(i) for i in range(1, 22)]
-    input_file_column_names = index_columns_names + operational_settings_columns_names + sensor_measure_columns_names
-
-    cols_to_drop = ['OpSet3', 'SensorMeasure1', 'SensorMeasure5', 'SensorMeasure6', 'SensorMeasure10',
-                    'SensorMeasure14',
-                    'SensorMeasure16', 'SensorMeasure18', 'SensorMeasure19']
-
-    df_train = pd.read_csv('train_FD004.txt', delim_whitespace=True, names=input_file_column_names)
-
-    rul = pd.DataFrame(df_train.groupby('UnitNumber')['Cycle'].max()).reset_index()
-    rul.columns = ['UnitNumber', 'max']
-    df_train = df_train.merge(rul, on=['UnitNumber'], how='left')
-    df_train['RUL'] = df_train['max'] - df_train['Cycle']
-    df_train.drop('max', axis=1, inplace=True)
-
-    df_test = pd.read_csv('test_FD004.txt', delim_whitespace=True, names=input_file_column_names)
-
-    if (drop_cols == True):
-        df_train = df_train.drop(cols_to_drop, axis=1)
-        df_test = df_test.drop(cols_to_drop, axis=1)
-
-    y_true = pd.read_csv('RUL_FD004.txt', delim_whitespace=True, names=["RUL"])
-    y_true["UnitNumber"] = y_true.index
-
-    return df_train, df_test, y_true
